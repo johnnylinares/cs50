372e010eaf179df327af634d0928217be12344bc
johnnylinares <johnnylinares26@gmail.com>
2025-05-26 11:52:16 -0400

    Mario

---
+++ week-1_c/mario.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void print_row(int n);
+
+int main(void)
+{
+    const int n = 3;
+    for (int i = 0; i < 3; i++)
+    {
+        print_row(n);
+    }
+}
+
+void print_row(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
